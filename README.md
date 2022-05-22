18e3819566394f515a324ca4fb317ca1b70c73b0
Davi Marques <davibaterabatera@hotmail.com>
2022-05-22 04:39:19 -0300

    :tada: Release commit

---
+++ README.md
@@ -0,0 +1,67 @@
+<img src="./PyForecast%20art.jpg" style="width: 100vw; " align="middle"/>
+
+&nbsp;
+
+# ☕ Description
+
+**PyForecast** is a simple weather forecast application made in Python with the help of the pandas and requests library. The system has 4 main functions: 
+* [x] <mark style="background-color: #00FF00; border-radius: 3px; padding-left: 2px; padding-right: 2px">Date Forecast</mark> - It is a function that receives from the user a location and a date, and returns a table with the weather forecast for that specified date and location.
+* [x] <mark style="background-color: #00FF7F; border-radius: 3px; padding-left: 2px; padding-right: 2px">Period Forecast</mark> - It is a function that receives from the user a location, a start date and an end date, and returns a table with the weather forecast for that specified period.
+* [x] <mark style="background-color: #DC143C; border-radius: 3px; padding-left: 2px; padding-right: 2px">Perfect Weather</mark> - It is a function in which the user chooses a weather that he considers perfect and the system returns a table with the days when the weather will be in the condition informed.
+* [x] Create a **.csv** file that stores web requests in a table with index, acquisition date, target date, minimum and maximum temperature, location and weather condition.
+
+The system can show the weather forecast for several cities in Brazil.
+
+&nbsp;
+
+# 📚 Required modules and languages:
+## Used language:
+* Python 🐍
+## Modules:
+* pandas (**pip install pandas**)
+* requests (**pip install requests**)
+* termcolor (**pip install termcolor**)
+* unidecode (**pip install unidecode**)
+* os (This module is native to Python, but can be installed by using this command **"pip install os"**)
+  
+The modules can also be installed using this command:
+
+```
+pip install requeriments.txt
+```
+
+&nbsp;
+
+# 🎢 Steps taken to reach this result:
+
+1. Requested the data with the HG Brasil API in JSON format.
+2. Then I filtered the information and turned it into a DataFrame.
+3. Export the dataframe to **.csv**
+4. Created the <mark style="background-color: #00FF00; border-radius: 3px; padding-left: 2px; padding-right: 2px">dateForecast( )</mark> function.  
+   * Returns a DataFrame with the weather forecast for a specific date.
+5. Created the <mark style="background-color: #00FF7F; border-radius: 3px; padding-left: 2px; padding-right: 2px">periodForecast( )</mark> function.
+   * Returns a DataFrame with the weather forecast between two different dates (MAX PERIOD: 10 days).
+6. Created the <mark style="background-color: #DC143C; border-radius: 3px; padding-left: 2px; padding-right: 2px">perfectWeather( )</mark> function.
+   * Returns a dataframe with the weather condition specified by the user.
+7. Created the Perfect Weather function menu.
+8. Created the Main menu.
+
+&nbsp;
+
+# 🤖 Let's see how it works:
+
+<br>
+<img src="./pyforecast_photos/show_pyforecast.png" style="width: 100vw; " align="middle"/>
+
+<br>
+
+# ⚠️ Observations to be considered:
+
+* In the <mark style="background-color: #00FF7F; border-radius: 3px; padding-left: 2px; padding-right: 2px">periodForecast( )</mark> function, the the api only shows the forecast of a maximum of 10 days from the current date.
+* You can navigate the menus using the indexes for each feature.
+
+&nbsp;
+
+# 📃 License
+### MIT License © [Daví Marques](https://github.com/marquesdavi). To learn more, check the license file by [CLICKING HERE](LICENSE.md).
+
---
+++ main.py
@@ -0,0 +1,142 @@
+#Importações
+import pandas as pd
+import requests
+from termcolor import colored
+from unidecode import unidecode
+import os
+
+
+class WeatherForecast:
+    def __init__(self) -> None:
+        pass
+          
+    #DataFrame    
+    def createDataFrame(self, citySearch):
+        try:
+            #Requisição
+            get_weather = requests.get(f"https://api.hgbrasil.com/weather?key=8c04a944&city_name={citySearch}")
+        
+        except Exception as erro:
+            print("Ocorreu um erro na requisição da API (╥﹏╥). Entre em contato com o suporte técnico 🔧", erro)
+
+        #Read the JSON, and convert to a DataFrame
+        RESPONSE = pd.read_json(get_weather.text)
+        self.cityResponse:str = RESPONSE["results"]["city"]
+        if unidecode(self.cityResponse.strip().upper()) != unidecode(citySearch.strip().upper()):
+            print("Cidade não encontrada! Padrão: São Paulo, SP")
+            citySearch = "São Paulo, SP"
+        forecastFilter = RESPONSE["results"]["forecast"]       
+        self.df = pd.DataFrame.from_dict(forecastFilter)
+        self.df.drop(columns=['condition'], inplace=True)
+        basedir = os.path.dirname(__file__)
+        if os.path.isfile(f"{basedir}/requisicoes.csv"):
+            header = False
+            
+        else:
+            header = "index,date,weekday,max,min,description,Data da aquisicao"
+        
+        with open(f"{basedir}/requisicoes.csv", "a+", encoding="utf-8") as f:
+            if header:
+                f.write(header + "\n")
+            ind = sum(1 for x in open(f"{basedir}/requisicoes.csv", "r")) -1
+            print(ind)                   
+            for value in forecastFilter:
+                ind += 1
+                f.write(f"{ind}, {value['date']}, {value['weekday']}, {value['max']}, {value['min']}, {value['description']}, {RESPONSE['results']['date']}\n")
+        return self.cityResponse
+    
+    #Function that returns the weather forecast for a specified date and location.
+    
+    def dateForecast(self, dateF):
+        self.df["localização"] = self.cityResponse
+        return self.df.loc[self.df["date"] == dateF]
+
+    #Function that returns the content between two specified dates
+
+    def periodForecast(self, start_date, end_date):
+        self.df["localização"] = self.cityResponse
+        mask = (self.df['date'] >= start_date) & (self.df['date'] <= end_date)
+        resultDf = self.df.loc[mask]
+        return resultDf
+    
+    #Function that allows you to show only the days when the weather will be favorable.
+
+    def perfectWeather(self, pWeather):
+        self.df["localização"] = self.cityResponse
+        return(self.df.loc[self.df["description"] == pWeather])
+
+#PerfectWeather function menu.
+   
+def menuPerfWeather(cityResponse):
+    print("\n")
+    print(colored("█" * 18 + " CLIMA PERFEITO " + "█" * 17, "blue", attrs=["bold"]))
+    print(f"Localização: {cityResponse}")
+    print("1 - Tempo limpo")
+    print("2 - Parcialmente nublado")
+    print("3 - Tempo nublado")
+    print("4 - Chuvas esparsas")
+    print("5 - Chuva")
+    print("0 - Voltar para o menu anterior.")
+    print(colored("█" * 51, "blue", attrs=["bold"]), "\n")
+
+#Main menu.
+
+def mainMenu(cityResponse):
+    print("\n")
+    print(colored("█" * 16 + ' PREVISÃO DO TEMPO ' + "█" * 16, "green", attrs=["bold"]))
+    print(f"Localização: {cityResponse}")
+    print("1 - Previsão por data")
+    print("2 - Previsão em um período de tempo")
+    print("3 - Clima perfeito")
+    print("0 - Sair do programa.")
+    print(colored("AVISO: Instruções de uso na documentação", "yellow"))
+    print(colored("█" * 51, "green", attrs=["bold"]), "\n")
+
+if __name__ == "__main__":
+    
+    try:
+        while True:
+            citySearch = input("\nDigite o nome de uma cidade (Ex: São Paulo, SP): ")
+            weather_forecast = WeatherForecast()
+            city_response = weather_forecast.createDataFrame(citySearch)
+            mainMenu(city_response)
+
+            chooseOption = int(input("\nEscolha uma opção: "))
+            
+            if chooseOption == 1:
+                date_forecast = weather_forecast.dateForecast(str(input("\nDigite uma data: ")))
+                print(date_forecast)
+            elif chooseOption == 2:
+                period_forecast = weather_forecast.periodForecast(str(input("\nDigite uma data: ")), str(input("Digite outra data: ")))
+                print(period_forecast)
+            elif chooseOption == 3:
+                
+                while True:
+                    menuPerfWeather(city_response)
+                
+                    chooseClimate = int(input("\nEscolha um clima(Ex: 1): "))
+                    
+                    if chooseClimate == 1:
+                         perfect_weather = weather_forecast.perfectWeather("Tempo limpo")
+                    elif chooseClimate == 2:
+                        perfect_weather = weather_forecast.perfectWeather("Parcialmente nublado")
+                    elif chooseClimate == 3:
+                        perfect_weather = weather_forecast.perfectWeather("Tempo nublado")
+                    elif chooseClimate == 4:
+                        perfect_weather = weather_forecast.perfectWeather("Chuvas esparsas")
+                    elif chooseClimate == 5:
+                        perfect_weather = weather_forecast.perfectWeather("Chuva")
+                    elif chooseClimate == 0:
+                        print("Voltando para o menu inicial...")
+                        break
+                    else:
+                        perfect_weather = "Opção inválida! Tente novamente..."
+                    print(perfect_weather)
+
+            elif chooseOption == 0:
+                print("Fechando o programa...")
+                break
+            else:
+                print("Opção inválida! Tente novamente...")
+    except Exception as erro:
+        print("Ocorreu um erro (╥﹏╥). Entre em contato com o suporte técnico 🔧", erro)
